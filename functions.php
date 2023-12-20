c47de6ffd5cf143f48a6ac72c5e1ddbd95e61af6
gqqnbig <gqqnb2005@gmail.com>
2023-12-19 22:25:16 -0800

    Hide comments form

--- functions.php
+++ functions.php
@@ -17,4 +17,21 @@ function use_jquery_from_cdn () {
 	wp_deregister_script('jquery');
 	wp_register_script('jquery', "//code.jquery.com/jquery-$ver.min.js", false, $ver);
 }
+
+add_action( 'comment_form_before', 'hide_comment_form_before' );
+
+function hide_comment_form_before() {
+	echo '<div>鉴于大量垃圾留言，留言表单已经被关闭。请发邮件到<span id="comment-email1" tabindex="0">antitrust</span>@<span id="comment-email2" tabindex="0">ftc.gov</span>发表评论。</div>';
+	echo '<script>';
+	echo 'jQuery("#comment-email1").one("mouseover focus", function(){this.innerText="gqqnb2005"});';
+	echo 'jQuery("#comment-email2").one("mouseover focus", function(){this.innerText="gmail.com"});';
+	echo '</script>';
+	echo '<div style="width: 1px; height: 1px; overflow: hidden;">';
+}
+
+add_action( 'comment_form_after', 'hide_comment_form_after' );
+
+function hide_comment_form_after() {
+    echo '</div>';
+}
 ?>
